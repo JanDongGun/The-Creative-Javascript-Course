14dd8d0c816ae6224f75ed0b0c12a692126416e3
jandonggun <donggontrannguyen@gmail.com>
2020-12-03 22:19:20 +0700

    finish basic

--- 01.TheBasics/index.js
+++ 01.TheBasics/index.js
@@ -1,1 +1,26 @@
-alert("Gon ne");
+console.log("erro");
+
+const Gon = {
+  name: "gon",
+  age: "20",
+  playGame: function () {
+    console.log("Lien Minh Huyen Thoai");
+  },
+};
+
+Gon.playGame();
+
+const names = ["Gon", "Tran", "GonVaTran", "laptop"];
+
+for (const name of names) {
+  console.log(name);
+}
+
+names.forEach((name) => console.log(name));
+
+for (const key in Gon) {
+  if (Gon.hasOwnProperty(key)) {
+    const element = Gon[key];
+    console.log(element);
+  }
+}
