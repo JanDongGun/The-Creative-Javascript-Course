147c735fa90b9106085e5248649464ce9c1341d4
jandonggun <donggontrannguyen@gmail.com>
2020-12-22 16:07:17 +0700

    add sound

--- 06.Beatmaker/app.js
+++ 06.Beatmaker/app.js
@@ -16,8 +16,26 @@ class Drumkit{
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
+        activeBars.forEach((bar) => {
+            bar.style.animation = "playTrack 0.3s alternate ease-in-out 2";
+
+            if (bar.classList.contains('active')) {
+                if (bar.classList.contains('kick-pad')) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains('snare-pad')) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains('hihat-pad')) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
-        console.log(activeBars);
+       
     }
 
     start() {
@@ -32,6 +50,9 @@ const drumKit = new Drumkit();
 
 drumKit.pads.forEach((pad) => {
     pad.addEventListener('click', drumKit.activePad);
+    pad.addEventListener('animationend', function () {
+        this.style.animation = "";
+    })
 });
 
 drumKit.playBtn.addEventListener('click', () => {
