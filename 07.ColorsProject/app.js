8211ffcef3681a0350add8d6bf771815741565cd
jandonggun <donggontrannguyen@gmail.com>
2020-12-24 15:06:50 +0700

    coloring our input

--- 07.ColorsProject/app.js
+++ 07.ColorsProject/app.js
@@ -20,6 +20,15 @@ function randomColors() {
 
     //   check for contrast
     checkTextContrast(randomColor, hexText);
+    // initial colorize sliders
+
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 randomColors();
@@ -32,6 +41,27 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //Scale Saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  // Scale brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  // Update Input color
+  saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+    0
+  )},${scaleBright(0.5)},${scaleBright(1)})`;
+
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75), rgb(75,204,75), rgb(75,204,204),rgb(75,75,204), rgb(204,75,204),rgb(204,75,75) )`;
 }
 
 generationBtn.addEventListener("click", randomColors);
