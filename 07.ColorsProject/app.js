6750c62242b25dff2dba4b1c3b0682eac0a82d71
jandonggun <donggontrannguyen@gmail.com>
2020-12-24 14:24:32 +0700

    add check constrast

--- 07.ColorsProject/app.js
+++ 07.ColorsProject/app.js
@@ -1,7 +1,7 @@
 // Global selections and variables
 
 const colorDivs = document.querySelectorAll(".color");
-const generationBtn = document.querySelectorAll(".generate");
+const generationBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 
@@ -17,7 +17,21 @@ function randomColors() {
 
     div.style.backgroundColor = randomColor;
     hexText.innerHTML = randomColor;
+
+    //   check for contrast
+    checkTextContrast(randomColor, hexText);
   });
 }
-
 randomColors();
+
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+
+  if (luminance > 0.5) {
+    text.style.color = "black";
+  } else {
+    text.style.color = "white";
+  }
+}
+
+generationBtn.addEventListener("click", randomColors);
