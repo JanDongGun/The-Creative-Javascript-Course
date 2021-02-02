518ead29adff14e25d30a21c86425ab01533f06b
jandonggun <donggontrannguyen@gmail.com>
2021-02-02 18:57:28 +0700

    add cursor animation

--- 08.WebsiteProject/app.js
+++ 08.WebsiteProject/app.js
@@ -60,4 +60,35 @@ function animateSlides() {
   });
 }
 
+let mouse = document.querySelector(".cursor");
+let mouseText = mouse.querySelector("span");
+
+function cursor(e) {
+  mouse.style.top = e.pageY + "px";
+  mouse.style.left = e.pageX + "px";
+}
+
+function activeCursor(e) {
+  const item = e.target;
+
+  if (item.id === "logo" || item.classList.contains("burger")) {
+    mouse.classList.add("nav-active");
+  } else {
+    mouse.classList.remove("nav-active");
+  }
+
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    mouseText.innerText = "Tap";
+    gsap.to(".title-swipe", 1, { y: "0%" });
+  } else {
+    mouse.classList.remove("explore-active");
+    mouseText.innerText = "";
+    gsap.to(".title-swipe", 1, { y: "100%" });
+  }
+}
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
+
 animateSlides();
