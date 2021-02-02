7d53e4f440f663955ed0082e6e57e855b65411cb
jandonggun <donggontrannguyen@gmail.com>
2021-02-02 16:26:49 +0700

    add scroll effect

--- 08.WebsiteProject/app.js
+++ 08.WebsiteProject/app.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
   // Init controller
@@ -9,7 +10,7 @@ function animateSlides() {
   const nav = document.querySelector(".nav-header");
 
   // loop over each slide
-  sliders.forEach((slide) => {
+  sliders.forEach((slide, index, slides) => {
     const revealImg = slide.querySelector(".reveal-img");
     const img = slide.querySelector("img");
     const revealText = slide.querySelector(".reveal-text");
@@ -19,9 +20,43 @@ function animateSlides() {
     });
 
     slideT1.fromTo(revealImg, { x: "0%" }, { x: "100%" });
-    slideT1.fromTo(img, { scale: 1.5 }, { scale: 1 }, "-=1");
+    slideT1.fromTo(img, { scale: 2 }, { scale: 1 }, "-=1");
     slideT1.fromTo(revealText, { x: "0%" }, { x: "100%" }, "-=0.75");
     slideT1.fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
+
+    slideScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      triggerHook: 0.25,
+      reverse: false,
+    })
+      .setTween(slideT1)
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "slide",
+      })
+      .addTo(controller);
+
+    const pageT1 = gsap.timeline();
+    let nextSlide = slides.length - 1 === index ? "end" : slides[index + 1];
+    pageT1.fromTo(nextSlide, { y: "0%" }, { y: "50%" });
+    pageT1.fromTo(slide, { opacity: 1, scale: 1 }, { opacity: 0, scale: 0.5 });
+    pageT1.fromTo(nextSlide, { y: "50%" }, { y: "0%" }, "-=0.5");
+
+    pageScene = new ScrollMagic.Scene({
+      triggerElement: slide,
+      duration: "100%",
+      triggerHook: 0,
+    })
+      .addIndicators({
+        colorStart: "white",
+        colorTrigger: "white",
+        name: "page",
+        indent: 200,
+      })
+      .setPin(slide, { pushFollowers: false })
+      .setTween(pageT1)
+      .addTo(controller);
   });
 }
 
