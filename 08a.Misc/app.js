1bcb3c8424a1d68bbbcac571016a72bfb2644b6a
jandonggun <donggontrannguyen@gmail.com>
2021-02-04 07:58:31 +0700

    deconstruct

---
+++ 08a.Misc/app.js
@@ -0,0 +1,14 @@
+let juice = ["banana", "apple", "mango"];
+
+let [banana, ...juices] = juice;
+
+juice = [...juice, "gon"];
+
+let gon = {
+  name: "gon",
+  age: "21",
+  photo: [1, 2, 3, 4, 5, 6],
+};
+
+let { photo } = gon;
+console.log(photo);
