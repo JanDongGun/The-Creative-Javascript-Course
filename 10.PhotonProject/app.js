41c25ba167c78e5c89d071d99ae22d4cfc8be42c
jandonggun <donggontrannguyen@gmail.com>
2021-02-05 08:21:36 +0700

    fetch api

---
+++ 10.PhotonProject/app.js
@@ -0,0 +1,30 @@
+const auth = "563492ad6f91700001000001ee22167b972f463aa1774618449c85b4";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitBtn = document.querySelector(".submit-btn");
+
+let searchValue;
+
+async function curatedPhotos() {
+  const dataFetch = await fetch(
+    "https://api.pexels.com/v1/curated?per_page=15",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+      <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
