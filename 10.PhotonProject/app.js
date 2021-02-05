eb9b9a9dc96b83bbdbe75157067030d3a195bd92
jandonggun <donggontrannguyen@gmail.com>
2021-02-05 08:36:05 +0700

    search photo

--- 10.PhotonProject/app.js
+++ 10.PhotonProject/app.js
@@ -1,9 +1,19 @@
 const auth = "563492ad6f91700001000001ee22167b972f463aa1774618449c85b4";
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
-const submitBtn = document.querySelector(".submit-btn");
+const form = document.querySelector(".search-form");
 
 let searchValue;
+
+searchInput.addEventListener("input", updateInput);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchPhotos(searchValue);
+});
+
+function updateInput(e) {
+  searchValue = e.target.value;
+}
 
 async function curatedPhotos() {
   const dataFetch = await fetch(
@@ -23,6 +33,28 @@ async function curatedPhotos() {
     galleryImg.classList.add("gallery-img");
     galleryImg.innerHTML = `<img src=${photo.src.large}></img>
       <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+async function searchPhotos(query) {
+  const dataFetch = await fetch(
+    `https://api.pexels.com/v1/search?query=${query}&per_page=15`,
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    }
+  );
+
+  const data = await dataFetch.json();
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src=${photo.src.large}></img>
+          <p>${photo.photographer}</p>`;
     gallery.appendChild(galleryImg);
   });
 }
